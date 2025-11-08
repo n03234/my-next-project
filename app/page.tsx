45313b7d83068b5529b5199e881088a5b73ff735
KTC <ktc@example.com>
2025-11-08 16:27:58 +0900

    初期設定の追加とスタイルシートの作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image"
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <div className={styles.description}>
-                <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>app/page.tsx</code>
-                </p>
+    　<div>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです。</p>
+    </div>
+    );
 }
