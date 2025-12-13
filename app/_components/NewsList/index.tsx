1a1494e2a5cfb22ad62abc62b488aa88991fc404
KTC <ktc@example.com>
2025-12-13 15:06:39 +0900

    feat: MicroCMSからメンバーとニュースのリストを取得する機能を追加し、関連する定数を定義

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import styles from "./index.module.css";
 import Category from "../Category";
 import Date from "../Date";
@@ -20,13 +19,23 @@ export default function NewsList({ news }: Props) {
             {news.map((article) => (
                 <li key={article.id} className={styles.list}>
                     <Link href={`/news/${article.id}`} className={styles.link}>
-                        <Image
-                          className={styles.image}
-                          src="/no-image.png"
-                          alt="No Image"
-                          width={1200}
-                          height={630}
-                        />
+                        {article.thumbnail ? (
+                            <Image
+                                src={article.thumbnail.url}
+                                alt=""
+                                width={article.thumbnail.width}
+                                height={article.thumbnail.height}
+                                className={styles.image}
+                            />
+                        ) : (
+                            <Image
+                            className={styles.image}
+                            src="/no-image.png"
+                            alt="no image"
+                            width={1200}
+                            height={630}
+                            />
+                        )}
                         <dl className={styles.content}>
                             <dt className={styles.title}>{article.title}</dt>
                             <dd className={styles.meta}>
